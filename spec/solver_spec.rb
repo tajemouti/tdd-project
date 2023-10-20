df170b007c974d22c3c5deaefb31c45fbc718636
tajemouti <tajemouti@gmail.com>
2023-10-20 12:10:58 +0100

    Write unit tests for the factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,4 +1,18 @@
 require './solver'
 
 describe Solver do
+  describe '#factorial' do
+    it 'returns 1 for input 0' do
+      expect(Solver.factorial(0)).to eq(1)
+    end
+
+    it 'returns the correct factorial for positive integers' do
+      expect(Solver.factorial(5)).to eq(120)
+      expect(Solver.factorial(10)).to eq(3_628_800)
+    end
+
+    it 'raises an exception for negative integers' do
+      expect { Solver.factorial(-1) }.to raise_error(ArgumentError)
+    end
+  end
 end
