520e696b859117685c3236f9b42c16352583bf93
mumbereh <siirasonh@gmail.com>
2023-10-20 12:18:13 +0100

    Create the factorial method

--- solver.rb
+++ solver.rb
@@ -1,2 +1,9 @@
 class Solver
+    def self.factorial(n)
+        raise ArgumentError, 'Input must be a non-negative integer' if n < 0
+    
+        result = 1
+        (1..n).each { |i| result *= i }
+        result
+    end
 end
