9a3f4ae48bfbe4f58c9f53aee9d798899767a6eb
tajemouti <tajemouti@gmail.com>
2023-10-20 12:28:08 +0100

    Write unit tests for the fizzbuzz method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -22,4 +22,26 @@ describe Solver do
       expect(Solver.reverse('world')).to eq('dlrow')
     end
   end
+
+  describe '#fizzbuzz' do
+    it 'returns "fizz" when N is divisible by 3' do
+      expect(Solver.fizzbuzz(3)).to eq('fizz')
+      expect(Solver.fizzbuzz(6)).to eq('fizz')
+    end
+
+    it 'returns "buzz" when N is divisible by 5' do
+      expect(Solver.fizzbuzz(5)).to eq('buzz')
+      expect(Solver.fizzbuzz(10)).to eq('buzz')
+    end
+
+    it 'returns "fizzbuzz" when N is divisible by both 3 and 5' do
+      expect(Solver.fizzbuzz(15)).to eq('fizzbuzz')
+      expect(Solver.fizzbuzz(30)).to eq('fizzbuzz')
+    end
+
+    it 'returns N as a string for other cases' do
+      expect(Solver.fizzbuzz(7)).to eq('7')
+      expect(Solver.fizzbuzz(11)).to eq('11')
+    end
+  end
 end
