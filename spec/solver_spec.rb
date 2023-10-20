c4892872e0f71826458fd5adc11f70650853fa0c
tajemouti <tajemouti@gmail.com>
2023-10-20 12:24:17 +0100

    Write unit test for the reverse method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -15,4 +15,11 @@ describe Solver do
       expect { Solver.factorial(-1) }.to raise_error(ArgumentError)
     end
   end
+
+  describe '#reverse' do
+    it 'returns the reversed word' do
+      expect(Solver.reverse('hello')).to eq('olleh')
+      expect(Solver.reverse('world')).to eq('dlrow')
+    end
+  end
 end
